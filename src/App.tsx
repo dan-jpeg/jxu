6e7d6c48b1527ef2cd39edd66cac74521857f0ad
dan <dancr.wley@gmail.com>
2024-10-04 14:22:52 -0400

    styling fixes for small screen

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
     return (
         <div className="App">
-            <div className="flex flex-col  min-h-screen ">
+            <div className="flex flex-col ">
                 <main className="flex-grow   ">
                     <Bonjour />
 
--- src/pages/Bonjour.tsx
+++ src/pages/Bonjour.tsx
@@ -36,7 +36,7 @@ const Bonjour: React.FC = () => {
     // }, []);
 
     return (
-        <div className="min-h-screen w-full bg-white flex items-center justify-center overflow-hidden flex-col">
+        <div className=" min-h-screen w-full bg-white flex items-center justify-center overflow-hidden flex-col">
             <div className="flex mx-4 max-h-[60vh] md:max-h-[60vh] max-w-screen-md w-full mb-4">
                 <ImageCycler images={jxuimages} interval={1424}/>
             </div>
